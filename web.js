07027cf067e57e5b40f7e2fe4783c979eedec9a3
jin xiao <594655291@qq.com>
2013-07-19 06:11:05 +0000

    Added readFile

--- web.js
+++ web.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var app = express();
+var fs = require('fs');
 app.use(express.logger());
 
+
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(fs.readFileSync(index.html).toString());
 });
 
 var port = process.env.PORT || 5000;
